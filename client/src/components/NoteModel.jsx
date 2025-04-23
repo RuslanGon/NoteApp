b2b7b60230025012c20a4a0825e30e266f95efcb
RuslanGon <ruslan441006@gmail.com>
2025-04-23 17:21:49 +0300

    wer

--- client/src/components/NoteModel.jsx
+++ client/src/components/NoteModel.jsx
@@ -1,14 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const NoteModel = () => {
+const NoteModel = ({closeModal}) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem('token');
+  
+      const response = await axios.post(
+        "http://localhost:5000/api/note/add",
+        { title, description },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+  
+      console.log("Note added successfully:", response.data);
+      setSuccessMessage("Note added! Redirecting...");
+      setTimeout(() => navigate("/"), 1500); 
+      closeModal()
+    } catch (error) {
+      console.error("Failed to add note:", error);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold text-center text-blue-600 mb-6">
           Add New Note
         </h2>
-        <form className="space-y-4">
+        <form onClick={handleSubmit} className="space-y-4">
           <div>
             <label
               htmlFor="title"
@@ -17,7 +48,9 @@ const NoteModel = () => {
               Title:
             </label>
             <input
+              onChange={(e) => setTitle(e.target.value)}
               type="text"
+              value={title}
               id="title"
               placeholder="Note title"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
@@ -31,6 +64,8 @@ const NoteModel = () => {
               Description:
             </label>
             <textarea
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
               id="description"
               rows="4"
               placeholder="Note description"
@@ -44,14 +79,16 @@ const NoteModel = () => {
             >
               Add Note
             </button>
-            
           </div>
-          <Link
-              to="/"
-              className="bg-red-600 hover:bg-red-700 text-black py-2  rounded-lg transition duration-300"
-            >
-              Cancel
-            </Link>
+          <button
+          onClick={closeModal}
+            className="bg-red-400 hover:bg-red-700 text-black py-2 px-4  rounded-lg transition duration-300 cursor-pointer"
+          >
+            Cancel
+          </button>
+          {successMessage && (
+            <p className="text-green-600 text-center">{successMessage}</p>
+          )}
         </form>
       </div>
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,38 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar.jsx'
 import NoteModel from '../components/NoteModel.jsx'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-
+const navigate = useNavigate()
 const [isModel, setIsModel] = useState(false)  
+const closeModal = () => {
+  setIsModel(false)
+}
+
+const addNote = async (e) => {
+  e.preventDefault();
+  try {
+    const token = localStorage.getItem('token');
+
+    const response = await axios.post(
+      "http://localhost:5000/api/note/add",
+      { title, description },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    console.log("Note added successfully:", response.data);
+    setTimeout(() => navigate("/"), 1500); 
+    closeModal()
+  } catch (error) {
+    console.error("Failed to add note:", error);
+  }
+}
 
   return (
     <div className='bg-gray-100 min-h-screen'>
@@ -13,7 +41,7 @@ const [isModel, setIsModel] = useState(false)
       className='fixed right-4 bottom-4 text-2xl bg-teal-500 text-white font-bold p-4 rounded-full'>
         +
       </button>
-      {isModel || <NoteModel />}
+      {isModel && <NoteModel closeModal={closeModal}/>}
     </div>
   )
 }
