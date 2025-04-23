565347f3a8f30169e180dfc0f7d8453fda14a782
RuslanGon <ruslan441006@gmail.com>
2025-04-23 15:26:27 +0300

    wer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar.jsx'
+import NoteModel from '../components/NoteModel.jsx'
 
 const Home = () => {
+
+const [isModel, setIsModel] = useState(false)  
+
   return (
-    <div>
+    <div className='bg-gray-100 min-h-screen'>
       <Navbar />
+      <button onClick={() => setIsModel(true)}
+      className='fixed right-4 bottom-4 text-2xl bg-teal-500 text-white font-bold p-4 rounded-full'>
+        +
+      </button>
+      {isModel && <NoteModel />}
     </div>
   )
 }
