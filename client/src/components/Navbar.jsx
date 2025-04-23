8186fefdcb41aaaca0c5e29dc48947c94ca733c5
RuslanGon <ruslan441006@gmail.com>
2025-04-23 14:37:29 +0300

    wer

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const handleLogout = () => {};
+
+  return (
+    <nav className="bg-gray-800 p-4 text-white flex justify-between items-center">
+      <div className="text-xl font-bold">
+        <Link to="/">NoteApp</Link>
+      </div>
+      <input
+        className="bg-gray-600 px-4 py-2 rounded"
+        type="text"
+        placeholder="Search notes..."
+      />
+      <div>
+        <span className="mr-4">user email</span>
+        <Link className="bg-blue-500 px-4 py-2 rounded mr-4" to="/login">
+          Login
+        </Link>
+        <Link className="bg-blue-500 px-4 py-2 rounded mr-4" to="/register">
+          Sing Up
+        </Link>
+        <button onClick={handleLogout} className="bg-red-400 px-4 py-2 rounded">
+          Logout
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
