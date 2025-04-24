eb808eaf535afc21b6206caf35b5be4ac3171d32
RuslanGon <ruslan441006@gmail.com>
2025-04-24 15:56:33 +0300

    wre

--- server/routes/note.js
+++ server/routes/note.js
@@ -34,4 +34,24 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.put('/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const updatedNote = await NoteModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+      });
+  
+      if (!updatedNote) {
+        return res.status(404).json({ message: 'Note not found' });
+      }
+      
+      res.status(200).json({ message: 'Note updated', note: updatedNote });
+    } catch (error) {
+      console.error('Error updating note:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+
 export default router;
